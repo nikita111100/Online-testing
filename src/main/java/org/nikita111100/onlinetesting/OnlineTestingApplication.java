bd46240a2f1e04f9a8d4957add745a8f881e856d
nikita111100 <50965855+nikita111100@users.noreply.github.com>
2020-11-27 01:33:12 +0400

    Сделаны задания #1 #2 #3

---
+++ src/main/java/org/nikita111100/onlinetesting/OnlineTestingApplication.java
@@ -0,0 +1,13 @@
+package org.nikita111100.onlinetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTestingApplication.class, args);
+	}
+
+}
