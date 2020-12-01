7fab37082af197c59337fd639c6d86d80c04f3a9
nikita111100 <50965855+nikita111100@users.noreply.github.com>
2020-12-01 11:54:39 +0400

    добавил correct_answer в possible_answer

--- etc/db/script_sql_testing_tables.sql
+++ etc/db/script_sql_testing_tables.sql
@@ -2,8 +2,8 @@ insert into test (id,name,theme)
 values ('1','How well do you know java','programming');
 insert into questions (id,text,test_id)
 values ('1','что такое java?','1');
-insert into possible_answer (id,text_answer,questions_id)
-values ('1','язык программирования','1');
+insert into possible_answer (id,text_answer,correct_answer,questions_id)
+values ('1','язык программирования','1','1');
 insert into user (id,name,role)
 values ('1','Nikita','user');
 select * from questions;
@@ -15,3 +15,5 @@ insert into answer_question (id,possible_answer_question_id,answer_test_id)
 values ('1','1','1');
 select * from answer_test where user_id = '1';
 select * from  answer_question, answer_test where user_id = '1' and answer_test_id = '1';
+select  * from answer_question inner join possible_answer
+on  possible_answer.id  =  answer_question.possible_answer_question_id and correct_answer = '1' ;
