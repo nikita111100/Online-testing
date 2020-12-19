55ea8ba944e8b0a07860db1481e6394c850a3d92
nikita111100 <50965855+nikita111100@users.noreply.github.com>
2020-12-19 15:40:33 +0400

    Добавлен UI

--- src/main/java/org/nikita111100/onlinetesting/controller/QuestionController.java
+++ src/main/java/org/nikita111100/onlinetesting/controller/QuestionController.java
@@ -4,39 +4,62 @@ import org.nikita111100.onlinetesting.model.persistent.Question;
 import org.nikita111100.onlinetesting.model.persistent.Test;
 import org.nikita111100.onlinetesting.service.QuestionService;
 import org.nikita111100.onlinetesting.service.TestService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
-@RestController
+@Controller
 public class QuestionController {
-    private final TestService testService;
-    private final QuestionService questionService;
+  private final QuestionService questionService;
+  private final TestService testService;
 
-    public QuestionController(TestService testService, QuestionService questionService) {
-        this.testService = testService;
+    public QuestionController(QuestionService questionService, TestService testService) {
         this.questionService = questionService;
+        this.testService = testService;
     }
 
 
-    @RequestMapping("/find")
-    public void getQuestion (){
-        Question question = questionService.findById(1l);
+    @GetMapping("/questions")
+    public String findAll (Model model){
+        List<Question> questions = questionService.findAll();
+        List<Test> tests = testService.findAll();
+        model.addAttribute("tests",tests);
+        model.addAttribute("questions", questions);
+        return "question-list";
     }
 
-    @RequestMapping("/save")
-    public void saveQuestion (){
-        Test test = testService.findById(1l);
-        Question question = new Question(1l,"Что такое java",test);
+    @GetMapping("/question-create")
+    public String createQuestionForm(Question question) {
+        return "question-create";
+    }
+
+    @PostMapping("/question-create")
+    public String createQuestion(Question question) {
         questionService.saveQuestion(question);
+        return "redirect:/questions";
     }
 
-    @RequestMapping("/delete")
-    public void deleteQuestion (Question question){
-        questionService.deleteById(1l);
+    @GetMapping("question-delete/{id}")
+    public String deleteQuestion(@PathVariable("id") Long id) {
+        questionService.deleteById(id);
+        return "redirect:/questions";
     }
-    @RequestMapping("/list")
-    public void testNamedQuery(){
-        questionService.findAll();
+
+    @GetMapping("/question-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id,Model model) {
+        Question question = questionService.findById(id);
+        model.addAttribute("question", question);
+        return "/question-update";
+    }
+
+    @PostMapping("/question-update")
+    public String updateQuestion(Question question){
+        questionService.saveQuestion(question);
+        return "redirect:/questions";
     }
 }
