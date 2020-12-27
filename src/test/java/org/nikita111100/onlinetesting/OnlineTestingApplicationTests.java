86879bd2f6addb6b8c6fa0316eeee26496cffb94
Aleksey Likhachev <ocmwdt@gmail.com>
2020-12-27 12:55:17 +0400

    #14: minor fixes

--- src/test/java/org/nikita111100/onlinetesting/OnlineTestingApplicationTests.java
+++ src/test/java/org/nikita111100/onlinetesting/OnlineTestingApplicationTests.java
@@ -3,8 +3,10 @@ package org.nikita111100.onlinetesting;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OnlineTestingApplicationTests {
     @Test
     void contextLoads() {
